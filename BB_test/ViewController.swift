c02878699bbb3b64a82cab2ee9edc996e9982440
Payson Peabody <ppeabody@sifma.org>
2018-10-21 15:53:48 -0400

    added drawing in main view

--- BB_test/ViewController.swift
+++ BB_test/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let newShape = CAShapeLayer()
+        view.layer.addSublayer(newShape)
+        
+        let path = UIBezierPath(ovalIn: CGRect(x: 30, y: 30, width: 150, height: 150))
+        path.fill()
+        newShape.path = path.cgPath
     }
 
 
